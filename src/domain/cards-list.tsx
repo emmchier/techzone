3091b536c82bca2ff70e0b30aaf06d6a37eca981
emmchier <emmchier>
2022-03-05 20:20:23 -0300

    domain folder added

---
+++ src/domain/cards-list.tsx
@@ -0,0 +1,29 @@
+import { BrowseIcon, ChooseIcon, EnjoyIcon } from "../components/ui/icons";
+
+export const cardsList = [
+  {
+    id: 0,
+    imageDesk: "/walkthroug-1-desktop.png",
+    imageMob: "/walkthroug-1-responsive.png",
+    icon: <BrowseIcon />,
+    title: "1—browse",
+    description: "Browse our tech catalog with more than 20 top tech products",
+  },
+  {
+    id: 1,
+    imageDesk: "/walkthroug-2-desktop.png",
+    imageMob: "/walkthroug-2-responsive.png",
+    icon: <ChooseIcon />,
+    title: "2—choose",
+    description: "Exchange your hard earned AeroPoints for the item you want",
+  },
+  {
+    id: 2,
+    imageDesk: "/walkthroug-3-desktop.png",
+    imageMob: "/walkthroug-3-responsive.png",
+    icon: <EnjoyIcon />,
+    title: "3—enjoy!",
+    description:
+      "All done, you can relax! We’ll take care of delivery of your tech item!",
+  },
+];
