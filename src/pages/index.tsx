cb307574acf3cd14d3e71928263e3b44e976cbe4
emmchier <emmchier>
2022-03-05 19:06:37 -0300

    all icons as objects

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,22 @@
 import type { NextPage } from "next";
 import Image from "next/image";
 import Page from "../components/common/page";
+import {
+  AerolabIsotype,
+  AerolabLogo,
+  AerolaLogoCircle,
+  ArrowIcon,
+  BrowseIcon,
+  ChevronIcon,
+  ChooseIcon,
+  CloseIcon,
+  EnjoyIcon,
+  ErrorIcon,
+  GithubIcon,
+  SingleWave,
+  SuccessIcon,
+  WavesPattern,
+} from "../components/ui/icons";
 
 const Home: NextPage = () => {
   return (
@@ -9,13 +25,26 @@ const Home: NextPage = () => {
       description="Here you’ll be able to exchange all of your hard-earned Aeropoints and exchange them for cool tech."
       image="/metadata.png"
     >
-      <h1>Aerolab</h1>
-      <Image
+      <AerolabIsotype />
+      <AerolaLogoCircle />
+      <AerolabLogo />
+      <ArrowIcon />
+      {/* <WavesPattern />
+      <SingleWave /> */}
+      <BrowseIcon />
+      <ChevronIcon />
+      <ChooseIcon />
+      <CloseIcon />
+      <EnjoyIcon />
+      <GithubIcon />
+      <ErrorIcon />
+      <SuccessIcon />
+      {/* <Image
         src="/static/hero-desktop.png"
         alt="Vercel Logo"
         width={500}
         height={500}
-      />
+      /> */}
     </Page>
   );
 };
